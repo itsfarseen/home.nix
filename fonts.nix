718a2f363b078861948792e246c96bfbb37296a8
Farseen <oss@itsfarseen.anonaddy.me>
2022-05-01 23:27:26 +0530

    fonts: indian fonts

--- fonts.nix
+++ fonts.nix
@@ -8,5 +8,21 @@
     font-awesome_4
     open-sans
     public-sans
+    # Indian
+    lohit-fonts.assamese
+    lohit-fonts.bengali
+    lohit-fonts.devanagari
+    lohit-fonts.gujarati
+    lohit-fonts.gurmukhi
+    lohit-fonts.kannada
+    lohit-fonts.kashmiri
+    lohit-fonts.maithili
+    lohit-fonts.malayalam
+    lohit-fonts.marathi
+    lohit-fonts.nepali
+    lohit-fonts.odia
+    lohit-fonts.sindhi
+    lohit-fonts.tamil
+    lohit-fonts.telugu
   ];
 }
