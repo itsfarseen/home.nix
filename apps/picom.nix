c2cfce84e98f203f444a2e2a2965b7a68bc96995
Farseen <oss@itsfarseen.anonaddy.me>
2022-05-01 20:05:06 +0530

    picom: no blur, no rounded corners, subtle opacity

--- apps/picom.nix
+++ apps/picom.nix
@@ -6,24 +6,14 @@
     shadow = true;
     shadowOffsets = [ (-9) (-8) ];
     shadowOpacity = "0.3";
-    #blur = true;
     menuOpacity = "0.95";
     extraOptions = ''
-      corner-radius = 5.0;
       shadow-radius = 10;
-      # blur-method = "dual_kawase";
-      # blur-strength = 10;
       glx-no-stencil = true;
 
       opacity-rule = [
-        "96:!focused",
-        "98:focused",
-        "98:class_g = 'Code'",
-        "98:class_g = 'Alacritty'"
-      ];
-
-      rounded-corners-exclude = [
-        "class_g = 'Polybar'"
+        "99:!focused",
+        "100:focused",
       ];
 
       fading = true;
