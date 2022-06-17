675e614e15e8da783a5a86020e1bf64b215c19d0
Farseen <oss@itsfarseen.anonaddy.me>
2022-06-17 16:35:38 +0530

    packages: Add when-cli (https://github.com/mitsuhiko/when)

--- packages.nix
+++ packages.nix
@@ -1,5 +1,22 @@
 { config, pkgs, ... }:
-{ home.packages = with pkgs; [
+let
+  when-cli = pkgs.rustPlatform.buildRustPackage {
+    name = "when-cli";
+    cargoSha256 = "YWTE/t1mrSQ1AmjH3ZXObDQMIC6oIdOAeIKkrKPHgIE=";
+    # Using this build support function to fetch it from github
+    src = pkgs.fetchFromGitHub {
+      owner = "mitsuhiko";
+      repo = "when";
+      # The git tag to fetch
+      rev = "4cc12ce4dcd5f1a155ecb866c70809b2332b5172";
+      # Hashes must be specified so that the build is purely functional
+      sha256 = "AKBbfSHvEeRC2F7jBQLobAi5olXqAUyukKOkYcr25zo=";
+      fetchSubmodules = true;
+    };
+    nativeBuildInputs = [ pkgs.cargo ];
+  };
+in { 
+  home.packages = with pkgs; [
     acpilight
     discord
     tdesktop
@@ -33,5 +50,7 @@
     vlc
     mplayer
     mpv
+    #
+    when-cli
   ];
 }
