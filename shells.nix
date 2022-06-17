bba56e16c4044bb850757768ed0e80a0dc8d83d1
Farseen <oss@itsfarseen.anonaddy.me>
2022-06-17 16:36:26 +0530

    starship: remove prog lang version numbers

--- shells.nix
+++ shells.nix
@@ -5,6 +5,14 @@
 
   programs.fzf.enable = true;
   programs.starship.enable = true;
+  programs.starship.settings = {
+    git_metrics.disabled = false;
+    git_commit.disabled = false;
+    git_state.disabled = false;
+    rust.disabled = true;
+    nodejs.disabled = true;
+    package.disabled = true;
+  };
 
   programs.z-lua.enable = true;
   home.shellAliases = { 
